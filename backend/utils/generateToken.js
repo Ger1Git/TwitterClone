85c52749b91038306fbc5fe3712c0befc641ceb1
George Geru <george.g@machinas.com>
2025-01-13 15:54:08 +0200

    cookie config

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 10 * 24 * 60 * 60 * 1000,
         htppOnly: true,
-        secure: process.env.NODE_ENV !== 'development'
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'none'
     });
 };
 
